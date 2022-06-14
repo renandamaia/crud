0e6eb2e8fade071b82379065e92d2e2d312a7785
Renan da Maia <84993727+renandamaia@users.noreply.github.com>
2022-06-14 10:53:08 -0300

    Update tabela_teste.sql

--- tabela_teste.sql
+++ tabela_teste.sql
@@ -1,56 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Tempo de geração: 13/06/2022 às 15:23
--- Versão do servidor: 5.5.25-log
--- Versão do PHP: 5.6.40
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `arduino`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tabela_teste`
---
-
 CREATE TABLE `tabela_teste` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `codigo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Índices de tabelas apagadas
---
-
---
--- Índices de tabela `tabela_teste`
---
 ALTER TABLE `tabela_teste`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
---
-
---
--- AUTO_INCREMENT de tabela `tabela_teste`
---
-ALTER TABLE `tabela_teste`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
